dd698f3b358511be7a2001c0a250f7a2c51888c4
Guilherme Alves <guialvescarvalho.gac@gmail.com>
2023-02-28 17:22:41 -0300

    Method Reference

---
+++ src/academy/devdojo/maratonajava/javacore/ZZClambdas/test/MethodReferenceTest02.java
@@ -0,0 +1,18 @@
+package academy.devdojo.maratonajava.javacore.ZZClambdas.test;
+
+import academy.devdojo.maratonajava.javacore.ZZClambdas.dominio.Anime;
+import academy.devdojo.maratonajava.javacore.ZZClambdas.service.AnimeComparators;
+
+import java.util.ArrayList;
+import java.util.List;
+
+//Reference to an instance method of a particular object
+public class MethodReferenceTest02 {
+    public static void main(String[] args) {
+        AnimeComparators animeComparators = new AnimeComparators();
+        List<Anime> animeList = new ArrayList<>(List.of(new Anime("Berserk", 53), new Anime("One Piece", 900), new Anime("Naruto", 500), new Anime("Boku no Hero", 150)));
+        animeList.sort(animeComparators::compareByEpisodesNonStatic);
+//        animeList.sort((a1,a2) -> animeComparators.compareByEpisodesNonStatic(a1,a2));
+        System.out.println(animeList);
+    }
+}
