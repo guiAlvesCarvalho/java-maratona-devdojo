3be4763dd7e3da9a17db67ed208c1bbe1e8ca0a3
Guilherme Alves <guialvescarvalho.gac@gmail.com>
2023-03-02 16:13:53 -0300

    Optional

---
+++ src/academy/devdojo/maratonajava/javacore/ZZDoptional/test/OptionalTest02.java
@@ -0,0 +1,17 @@
+package academy.devdojo.maratonajava.javacore.ZZDoptional.test;
+
+import academy.devdojo.maratonajava.javacore.ZZDoptional.dominio.Manga;
+import academy.devdojo.maratonajava.javacore.ZZDoptional.repositorio.MangaRepository;
+
+import java.util.Optional;
+
+public class OptionalTest02 {
+    public static void main(String[] args) {
+        Optional<Manga> mangaByTitle = MangaRepository.findByTitle("Boku no Hero");
+        mangaByTitle.ifPresent(m -> m.setTitle("Boku no Hero 2"));
+        System.out.println(mangaByTitle);
+
+        MangaRepository.findById(2)
+                .orElseThrow(IllegalArgumentException::new);
+    }
+}
