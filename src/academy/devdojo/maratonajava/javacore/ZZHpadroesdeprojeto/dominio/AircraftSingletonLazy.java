8c742429f3053d46252cbd76ceae4ee320ca50f8
Guilherme Alves <guialvescarvalho.gac@gmail.com>
2023-03-14 20:11:27 -0300

    Design Patterns

---
+++ src/academy/devdojo/maratonajava/javacore/ZZHpadroesdeprojeto/dominio/AircraftSingletonLazy.java
@@ -0,0 +1,30 @@
+package academy.devdojo.maratonajava.javacore.ZZHpadroesdeprojeto.dominio;
+
+import java.util.HashSet;
+import java.util.Set;
+
+public final class AircraftSingletonLazy {
+    private static AircraftSingletonLazy INSTANCE;
+    private final Set<String> availableSeats = new HashSet<>();
+    private final String name;
+
+    private AircraftSingletonLazy(String name) {
+        this.name = name;
+    }
+
+    {
+        availableSeats.add("1A");
+        availableSeats.add("1B");
+    }
+
+    public static AircraftSingletonLazy getINSTANCE(){
+        if (INSTANCE == null) {
+            INSTANCE = new AircraftSingletonLazy("797-900");
+        }
+        return INSTANCE;
+    }
+
+    public boolean bookSeat(String seat) {
+        return availableSeats.remove(seat);
+    }
+}
