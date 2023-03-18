8d8f282ecd235ed00264089ae49f82df237d8966
Guilherme Alves <guialvescarvalho.gac@gmail.com>
2023-03-18 15:00:46 -0300

    JDBC ResultSet pt1

--- src/main/java/academy/devdojo/maratonajava/javacore/ZZIjdbc/test/ConnectionFactoryTest01.java
+++ src/main/java/academy/devdojo/maratonajava/javacore/ZZIjdbc/test/ConnectionFactoryTest01.java
@@ -1,18 +1,22 @@
 package academy.devdojo.maratonajava.javacore.ZZIjdbc.test;
 
-import academy.devdojo.maratonajava.javacore.ZZIjdbc.conn.ConnectionFactory;
 import academy.devdojo.maratonajava.javacore.ZZIjdbc.dominio.Producer;
-import academy.devdojo.maratonajava.javacore.ZZIjdbc.repository.ProducerRepository;
 import academy.devdojo.maratonajava.javacore.ZZIjdbc.service.ProducerService;
+import lombok.extern.log4j.Log4j2;
 
+@Log4j2
 public class ConnectionFactoryTest01 {
     public static void main(String[] args) {
-        Producer producer = Producer.builder().name("NHK").build();
-        Producer producerToUpdate = Producer.builder().id(1).name("MADHOUSE").build();
+//        Producer producer = Producer.builder().name("NHK").build();
+//        Producer producerToUpdate = Producer.builder().id(1).name("MADHOUSE").build();
 //        ProducerService.save(producer);
 //        ProducerService.delete(3);
 //        ProducerService.update(producerToUpdate);
-
+//        List<Producer> producers = ProducerService.findAll();
+//        List<Producer> producers = ProducerService.findByName("Mad");
+//        log.info("Producers found {}", producers);
+//        ProducerService.showProducerMetaData();
+        ProducerService.showDriverMetaData();
 
     }
 }
